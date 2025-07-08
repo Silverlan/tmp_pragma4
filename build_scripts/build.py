48eddccbc43a37d874910af8de1aad665f7b14a6
Silverlan <silverlann@gmail.com>
2025-07-08 12:03:08 +0200

    Update build.py

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -714,7 +714,7 @@ if with_pfm:
 		)
 		add_pragma_module(
 			name="pr_unirender",
-			commitSha="b29e8b3ad53c66311b344a0b4934395d70f93581",
+			commitSha="ebccb3b4583a11453e04302369b2e39cdf0abec7",
 			repositoryUrl="https://github.com/Silverlan/tmp_pr_cycles3.git"
 		)
 		add_pragma_module(
